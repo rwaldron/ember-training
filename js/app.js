5ff235fa42a69834779bf12390f5f0ccd5fc9135
tomhuda <tomhuda@tilde.io>
2013-02-10 23:30:10 -0800

    Step 5

--- js/app.js
+++ js/app.js
@@ -4,11 +4,18 @@
 window.App = Ember.Application.create();
 
 App.Router.map(function() {
+  this.resource('album', { path: '/album/:album_id' });
 });
 
-App.ApplicationRoute = Ember.Route.extend({
+App.IndexRoute = Ember.Route.extend({
   model: function() {
     return App.ALBUM_FIXTURES;
+  }
+});
+
+App.AlbumRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.ALBUM_FIXTURES.findProperty('id', params.album_id);
   }
 });
 
