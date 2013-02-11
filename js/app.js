0954143e20175f3579fb23712fd60ebd77ff6781
tomhuda <tomhuda@tilde.io>
2013-02-10 22:46:35 -0800

    Step 2

--- js/app.js
+++ js/app.js
@@ -6,4 +6,10 @@ window.App = Ember.Application.create();
 App.Router.map(function() {
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return App.ALBUM_FIXTURES;
+  }
+});
+
 })();
